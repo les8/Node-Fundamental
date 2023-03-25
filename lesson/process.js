20ac789330c4e0a5c5e8c56e0a10d4e1dbf0632f
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-03-25 10:35:47 +0300

    process

---
+++ lesson/process.js
@@ -0,0 +1,27 @@
+// Process
+// console.log(process.pid);
+
+
+// Cтандартные переменные окружения
+// console.log(process.env); 
+
+
+// Кастомные переменные
+// const dotenv = require('dotenv');
+// dotenv.config();
+// console.log(process.env.PORT);
+// console.log(process.env.NODE_ENV);
+
+
+//Аргументы
+// console.log(process.argv);
+
+
+//Завершение процесса приложения
+// if (Math.random() > 0.5) {
+//   console.log('Бесконечное выполнение');
+//   while(true) {}
+// } else {
+//   console.log('Выполнение программы завершено');
+//   process.exit()
+// }
